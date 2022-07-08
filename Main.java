c4dcdb109b1a4e5b4c05e11559e8ffa2a11e8e1c
varshit-yadav <varshit.ip@wissen.com>
2022-07-08 13:14:54 +0530

    first commmit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Calculator{
+
+	public int add(int a,int b){
+         return a+b;
+	}
+	public int sub(int a,int b){
+		return a-b;
+	}
+
+}
+
+public class Main{
+	public static void main(String[] args) {
+
+		Calculator c1=new Calculator();
+		System.out.println(c1.add(3,2));
+		System.out.println(c1.sub(3,2));
+	}
+}
