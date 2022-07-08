595565b1216dea59c523a1172e69d3a5b0488d30
varshit-yadav <varshit.ip@wissen.com>
2022-07-08 16:14:24 +0530

    division func. added

--- Main.java
+++ Main.java
@@ -9,6 +9,7 @@ class Calculator{
 	public int mul(int a,int b){
 		return a*b;
 	}
+	public int div(int a,int b)
 
 }
 
@@ -16,8 +17,9 @@ public class Main{
 	public static void main(String[] args) {
 
 		Calculator c1=new Calculator();
-		System.out.println(c1.add(3,2));
-		System.out.println(c1.sub(4,2));
-		System.out.println(c1.mul(4,2));
+		System.out.println("addition of 2 numbers "+c1.add(3,2));
+		System.out.println("substraction of 2 numbers "+c1.sub(4,2));
+		System.out.println("multiplication of 2 numbers "+c1.mul(4,2));
+		System.out.println("division of 2 numbers "+c1.div(4,2));
 	}
 }
